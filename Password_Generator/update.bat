cdcc4f2da688c6e1cda98fe19e077a38c921d3a7
yokito <112480670+yokito0305@users.noreply.github.com>
2022-11-16 18:12:06 +0800

    Add files via upload
    修正自動更新bug

--- Password_Generator/update.bat
+++ Password_Generator/update.bat
@@ -3,23 +3,22 @@ FOR /F "usebackq delims=" %%i in (`powershell -Command "& {[Environment]::GetFol
 
 chcp 65001
 
-echo 終止程序中...
-@echo off
+@echo 終止程序中...
 taskkill /f /im password_generator_win.exe
 
 if exist "%DOCUMENTSDIR%\Password Generator\tmp" (
 
-    echo 更新檔案中...
+    @echo 更新檔案中...
     @echo off
 
-    xcopy /s "%DOCUMENTSDIR%\Password Generator\tmp"\* .\ /y/k
+    xcopy /s "%DOCUMENTSDIR%\Password Generator\tmp"\* .\ /y
     rmdir /s/q "%DOCUMENTSDIR%\Password Generator\tmp"
 
-    echo 更新完畢!
+    @echo 更新完畢!
     @echo off
-    exit
 
 ) else (
-    echo 發生錯誤
+    @echo 發生錯誤
     @echo off
 )
+pause
