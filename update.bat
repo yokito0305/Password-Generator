b5f4141e36b762d6de795d1d13e181659d4d65a7
yokito <112480670+yokito0305@users.noreply.github.com>
2022-11-16 17:30:08 +0800

    Add files via upload

---
+++ update.bat
@@ -0,0 +1,25 @@
+﻿@echo off
+FOR /F "usebackq delims=" %%i in (`powershell -Command "& {[Environment]::GetFolderPath('MyDocuments')}"`) DO SET DOCUMENTSDIR=%%i
+
+chcp 65001
+
+echo 終止程序中...
+@echo off
+taskkill /f /im password_generator_win.exe
+
+if exist "%DOCUMENTSDIR%\Password Generator\tmp" (
+
+    echo 更新檔案中...
+    @echo off
+
+    xcopy /s "%DOCUMENTSDIR%\Password Generator\tmp"\* .\ /y/k
+    rmdir /s/q "%DOCUMENTSDIR%\Password Generator\tmp"
+
+    echo 更新完畢!
+    @echo off
+    exit
+
+) else (
+    echo 發生錯誤
+    @echo off
+)
